9d9e9ee106c54abf5e42a95a6e3e89da0b4768bc
sudheerkumarm <sudheerkumarm@gmail.com>
2014-06-18 11:34:22 +0530

    removed merge conflicts

--- src/main/java/com/test/app/App.java
+++ src/main/java/com/test/app/App.java
@@ -9,12 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-<<<<<<< HEAD
         System.out.println( "This is next line for testing" );
-
-=======
 	System.out.println( "I  am testing1" );
 	System.out.println( "I  am testing2" )
->>>>>>> bc9f18d6f2eb2306e4370efb2106f9fd245b5e7d
     }
 }
