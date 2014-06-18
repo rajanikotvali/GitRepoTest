9ac3c73f80e806eba82148e55447971554e748f8
rkotvali <rajanikanth.kotvali@valuelabs.net>
2014-06-18 12:33:45 +0530

    commiting to test auto build feature

--- src/main/java/com/test/app/App.java
+++ src/main/java/com/test/app/App.java
@@ -10,8 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "This is next line for testing" );
-	System.out.println( "I  am testing1" );
-	System.out.println( "I  am testing2" )
-	System.out.println(" This is other line for auto build");
+		System.out.println(" This is other line for auto build");
     }
 }
