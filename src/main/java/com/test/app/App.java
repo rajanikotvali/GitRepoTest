f933c8264d1cddb473ac0f47c417a20d6b35d6f4
rkotvali <rajanikanth.kotvali@valuelabs.net>
2014-06-18 15:05:46 +0530

    added few characters to sop statement

--- src/main/java/com/test/app/App.java
+++ src/main/java/com/test/app/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "This is next line for testing" );
-		System.out.println(" This is other line for auto build");
+        System.out.println( "This is next line for testing ***************************** " );
     }
 }
